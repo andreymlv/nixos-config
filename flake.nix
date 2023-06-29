5736391b5e583fd984a103b8100057651e588739
andreymlv <ndrmlv@ya.ru>
2023-06-29 19:34:56 +0300

    Rename thinkpad to chocolate

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
   inputs.home-manager.url = "gitlab:rycee/home-manager/release-23.05";
   outputs = { self, nixpkgs, home-manager }@inputs: {
-    nixosConfigurations.thinkpad-e490 = nixpkgs.lib.nixosSystem {
-      modules = [ ./machines/thinkpad-e490/configuration.nix ];
-      specialArgs.inputs = inputs;
+    nixosConfigurations.chocolate = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      modules = [ ./machines/chocolate/configuration.nix ];
+      specialArgs.inputs = inputs;
     };
   };
 }
